a4b06ba1878673f7525550c6cad7f191ae074d0c
kdyook <kdyook@mobilecen.co.kr>
2024-04-03 16:52:45 +0900

    feat_notice페이지 생성

--- web_study/main/admin.py
+++ web_study/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+# notice Model을 불러옴
+from .models import Notice
+
 # Register your models here.
+# admin이 Notice 접근
+admin.site.register(Notice)
--- web_study/main/models.py
+++ web_study/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+# Notice( Title, Contents )
+class Notice(models.Model):
+    title = models.CharField(max_length=50)
+    contents = models.TextField
+
+    # Notice Object라고 나오는 제목을 title로 나타나게 수정
+    def __str__(self):
+        return self.title
